9c6f4bc63915fcfb71edae7f80d15c9a8f0db430
Heejae Chang <hechang@microsoft.com>
2015-09-01 15:09:59 -0700

    added initial aggregator to table entries factory

--- src/VisualStudio/Core/Def/Implementation/TableDataSource/TableEntriesFactory.cs
+++ src/VisualStudio/Core/Def/Implementation/TableDataSource/TableEntriesFactory.cs
@@ -1,15 +1,17 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using Microsoft.VisualStudio.Shell.TableManager;
+using Microsoft.VisualStudio.Text;
 
 namespace Microsoft.VisualStudio.LanguageServices.Implementation.TableDataSource
 {
     internal class TableEntriesFactory<TData> : ITableEntriesSnapshotFactory
     {
         private readonly AbstractTableDataSource<TData> _source;
-        private readonly AbstractTableEntriesSource<TData> _entriesSource;
+        private readonly AggregatedEntriesSource _entriesSources;
         private readonly WeakReference<ITableEntriesSnapshot> _lastSnapshotWeakReference = new WeakReference<ITableEntriesSnapshot>(null);
 
         private int _lastVersion = 0;
@@ -20,7 +22,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.TableDataSource
         public TableEntriesFactory(AbstractTableDataSource<TData> source, AbstractTableEntriesSource<TData> entriesSource)
         {
             _source = source;
-            _entriesSource = entriesSource;
+            _entriesSources = new AggregatedEntriesSource(entriesSource);
         }
 
         public int CurrentVersionNumber
@@ -47,7 +49,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.TableDataSource
                 }
 
                 var itemCount = _lastItemCount;
-                var items = _entriesSource.GetItems();
+                var items = _entriesSources.GetItems();
 
                 if (items.Length != itemCount)
                 {
@@ -81,7 +83,7 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.TableDataSource
                 //
                 // this is a kind of sanity check to reduce number of times we return wrong snapshot.
                 // but the issue will quickly fixed up since diagnostic service will drive error list to latest snapshot.
-                var items = _entriesSource.GetItems();
+                var items = _entriesSources.GetItems();
                 if (items.Length != _lastItemCount)
                 {
                     _source.Refresh(this);
@@ -126,10 +128,92 @@ namespace Microsoft.VisualStudio.LanguageServices.Implementation.TableDataSource
 
         private ITableEntriesSnapshot CreateSnapshot(int version, ImmutableArray<TData> items)
         {
-            var snapshot = _entriesSource.CreateSnapshot(version, items, _entriesSource.GetTrackingPoints(items));
+            var snapshot = _entriesSources.CreateSnapshot(version, items, _entriesSources.GetTrackingPoints(items));
             _lastSnapshotWeakReference.SetTarget(snapshot);
 
             return snapshot;
+        }
+
+        private class AggregatedEntriesSource : AbstractTableEntriesSource<TData>
+        {
+            private readonly SourceCollections _sources;
+
+            public AggregatedEntriesSource(AbstractTableEntriesSource<TData> primary)
+            {
+                _sources = new SourceCollections(primary);
+            }
+
+            public override ImmutableArray<TData> GetItems()
+            {
+                if (_sources.Primary != null)
+                {
+                    return _sources.Primary.GetItems();
+                }
+
+                return ImmutableArray<TData>.Empty;
+            }
+
+            public override ImmutableArray<ITrackingPoint> GetTrackingPoints(ImmutableArray<TData> items)
+            {
+                if (_sources.Primary != null)
+                {
+                    return _sources.Primary.GetTrackingPoints(items);
+                }
+
+                return ImmutableArray<ITrackingPoint>.Empty;
+            }
+
+            public override AbstractTableEntriesSnapshot<TData> CreateSnapshot(int version, ImmutableArray<TData> items, ImmutableArray<ITrackingPoint> trackingPoints)
+            {
+                if (_sources.Primary != null)
+                {
+                    return _sources.Primary.CreateSnapshot(version, items, trackingPoints);
+                }
+
+                return null;
+            }
+
+            private struct SourceCollections
+            {
+                private AbstractTableEntriesSource<TData> _primary;
+                private List<AbstractTableEntriesSource<TData>> _sources;
+
+                public SourceCollections(AbstractTableEntriesSource<TData> primary) : this()
+                {
+                    _primary = primary;
+                }
+
+                public AbstractTableEntriesSource<TData> Primary
+                {
+                    get
+                    {
+                        if (_primary != null)
+                        {
+                            return _primary;
+                        }
+
+                        if (_sources.Count == 1)
+                        {
+                            return _sources[0];
+                        }
+
+                        return null;
+                    }
+                }
+
+                public List<AbstractTableEntriesSource<TData>> GetSources()
+                {
+                    if (_sources == null)
+                    {
+                        _sources = new List<AbstractTableEntriesSource<TData>>();
+                    }
+
+                    _sources.Add(_primary);
+                    _primary = null;
+
+                    return _sources;
+                }
+            }
         }
     }
 }
