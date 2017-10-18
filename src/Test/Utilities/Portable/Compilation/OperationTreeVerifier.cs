47871796d49d6237a56f831c1f42d65894aef845
Heejae Chang <hechang@microsoft.com>
2017-10-17 21:46:00 -0700

    log more than before

--- src/Test/Utilities/Portable/Compilation/OperationTreeVerifier.cs
+++ src/Test/Utilities/Portable/Compilation/OperationTreeVerifier.cs
@@ -25,6 +25,7 @@ namespace Microsoft.CodeAnalysis.Test.Utilities
         private const string indent = "  ";
         private string _currentIndent;
         private bool _pendingIndent;
+        private bool _logParent;
 
         public OperationTreeVerifier(Compilation compilation, IOperation root, int initialIndent)
         {
@@ -34,6 +35,7 @@ namespace Microsoft.CodeAnalysis.Test.Utilities
 
             _currentIndent = new string(' ', initialIndent);
             _pendingIndent = true;
+            _logParent = true;
         }
 
         public static void Verify(Compilation compilation, IOperation operation, string expectedOperationTree, int initialIndent = 0)
@@ -100,13 +102,41 @@ namespace Microsoft.CodeAnalysis.Test.Utilities
                 LogString(", IsImplicit");
             }
 
-
             LogString(")");
 
             // Syntax
-            LogString($" (Syntax: {GetSnippetFromSyntax(operation.Syntax)})");
+            LogString($" (Syntax: {GetKind(operation.Syntax)} {GetSnippetFromSyntax(operation.Syntax)})");
+
+            // Parent
+            if (_logParent)
+            {
+                LogString($" (Parent: {operation.Parent?.Kind}");
+
+                // log only once for the first operation.
+                _logParent = false;
+            }
 
             LogNewLine();
+
+            // children order
+            LogString($" Children Order: {GetChildOperationOrder(operation)}");
+
+            LogNewLine();
+        }
+
+        private string GetChildOperationOrder(IOperation operation)
+        {
+            return string.Join(",", operation.Children.Select(c => c.Kind.ToString()));
+        }
+
+        private string GetKind(SyntaxNode syntax)
+        {
+            if (syntax.Language == LanguageNames.CSharp)
+            {
+                return ((CSharp.CSharpSyntaxNode)syntax).Kind().ToString();
+            }
+
+            return ((VisualBasic.VisualBasicSyntaxNode)syntax).Kind().ToString();
         }
 
         private static string GetSnippetFromSyntax(SyntaxNode syntax)
